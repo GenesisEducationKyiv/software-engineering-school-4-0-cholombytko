d8f1e0061cdf65de0b3b2a691e38e4eab040a23d
kcholombytko <funnygungan@gmail.com>
2024-06-17 00:00:34 +0300

    refactor: refactor schedule service using solid
    This commit contains refactoring of schedule service using Dependency
    Inversion principle

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,9 +1,10 @@
+import { IScheduleService } from './interfaces/schedule-service.interface';
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { IMailingService } from 'src/email/interfaces/mailing-service.interface';
 
 @Injectable()
-export class ScheduleService {
+export class ScheduleService implements IScheduleService {
   constructor(private readonly mailingService: IMailingService) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_NOON)
