18d899b29e4dfc0fb3c2e76b51b7a5fa91d86359
kcholombytko <funnygungan@gmail.com>
2024-06-18 09:00:45 +0300

    refactor: addon to previous commit

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -2,13 +2,18 @@ import { IScheduleService } from './interfaces/schedule-service.interface';
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { IMailingService } from 'src/email/interfaces/mailing-service.interface';
+import { IRateService } from 'src/rate/interfaces/rate-service.interface';
 
 @Injectable()
 export class ScheduleService implements IScheduleService {
-  constructor(private readonly mailingService: IMailingService) {}
+  constructor(
+    private readonly mailingService: IMailingService,
+    private readonly rateService: IRateService,
+  ) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_NOON)
   public async scheduleEveryDayTask(): Promise<void> {
-    await this.mailingService.sendMailsToSubscribers();
+    const payload = await this.rateService.getExchangeRate();
+    await this.mailingService.sendMailsToSubscribers(payload);
   }
 }
