4ac657947caa4073632d79d4359abbee8ef09a49
kcholombytko <funnygungan@gmail.com>
2024-06-22 00:00:15 +0300

    feat: add new api url
    Add new free api url to get exchange rate

--- src/rate/rate.constants.ts
+++ src/rate/rate.constants.ts
@@ -1,2 +1,5 @@
 export const API_URL =
   'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?valcode=USD&json';
+
+export const API_2_URL =
+  'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json';
